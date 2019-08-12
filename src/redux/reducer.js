7b3ca0b5ed3b7dad8b06d11fb8b89064b8516ff0
Viktor He <52307907+Viktorhe@users.noreply.github.com>
2019-08-12 21:59:30 +0700

    Submit Skeleton

---
+++ src/redux/reducer.js
@@ -0,0 +1,11 @@
+const defaultState={
+    
+}
+
+const myReducer = (state=defaultState, action) => {
+    switch(action.type){
+        default:
+            return 0
+    }
+}
+export default myReducer
