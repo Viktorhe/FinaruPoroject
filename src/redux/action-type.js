8a02d02393c2b5266570b6eae680dc51d35f6505
Viktor He <viktor.he@yahoo.com>
2019-08-19 22:08:36 +0700

    Apply action and reducer

---
+++ src/redux/action-type.js
@@ -0,0 +1,13 @@
+export const USER = "USER";
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
+
+export const THREADS = "THREADS";
+export const ADD_THREAD = "ADD_THREAD";
+export const EDIT_THREAD = "EDIT_THREAD";
+export const DELETE_THREAD = "DELETE_THREAD";
+
+export const COMMENTS = "COMMENTS";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const DELETE_COMMENT = "DELETE_COMMENT";
